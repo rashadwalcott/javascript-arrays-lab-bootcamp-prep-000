821f693c3c8ac57d109ed29b7e370ee618162092
Rashad Walcott <rashad.walcott15@gmail.com>
2019-02-16 00:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function prependKitten(name){
   return [name,...kittens];
 }
 function removeLastKitten(){
- return kittens.slice(0,)
+ return kittens.slice(0, kittens.length)
 }
 
 function removeFirstKitten(){
